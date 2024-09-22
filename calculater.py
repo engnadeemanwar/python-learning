5eeecc2eafd102e0b0942f035546165969f53151
Engnadeemanwar <muhammadnadeemanwar95@gmail.com>
2024-09-22 08:56:25 +0500

    calculator through functions

--- calculater.py
+++ calculater.py
@@ -1,22 +1,37 @@
-a = int(input("Enter first value: "))
-b = int(input("Enter second value: "))
+def add(a , b):
+    c = a+b
+    return print("\nThe Addition of the numbers are. ", c, "\n")
+
+def mul(a , b):
+    c = a*b
+    return print("\nThe Multication of the numbers are. ", c, "\n")
+
+def sub(a , b):
+    c = a-b
+    return print("\nThe subtraction of the numbers are. ", c, "\n")
+
+def div(a , b):
+    c = a/b
+    return print("\nThe Division of the numbers are. ", c, "\n")
+
+def mean(a , b):
+    c = (a+b)/2    
+    return print("\nThe Mean value of the numbers are. ", c, "\n") 
+
+a = float(input("Enter first value: "))
+b = float(input("Enter second value: "))
 c : float
 option = int(input("\nFor multiplication press 1\nFor addition press 2\nFor subtraction press 3\nFor division press 4\nFor mean press 5 \n\n"))
 
 if option == 1:
-    c = a*b
-    print("\nThe Multication of the numbers are. ", c, "\n")
+    mul(a , b)
 elif option == 2:
-    c = a+b
-    print("\nThe Addition of the numbers are. ", c, "\n")
+    add(a, b)
 elif option == 3:
-    c = a-b
-    print("\nThe subtraction of the numbers are. ", c, "\n")
+    sub(a , b)
 elif option == 4:
-    c = a/b
-    print("\nThe Division of the numbers are. ", c, "\n")
-elif option == 5: 
-    c = (a+b)/2    
-    print("\nThe Mean value of the numbers are. ", c, "\n")
+    div(a , b)
+elif option == 5:
+    mean(a , b) 
 else:
     print("\nInvalid Option\n")
